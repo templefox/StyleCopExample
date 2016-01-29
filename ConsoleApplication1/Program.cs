bad73bf9b8aff8a6716bd3258d2dac63c5d5601b
templefox <unboundfox@163.com>
2016-01-29 23:33:25 +0800

    third

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace    ConsoleApplication1
 {
-    class Program
-    {
-        static void Main(string[] args)
+            class Program
+    {        //[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+                     public event Action<int> eventWithoutUpperCase;
+
+        //[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1303:ConstFieldNamesMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        public const string constWithLowerCase = "CONST";
+        
+        //[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1307:AccessibleFieldsMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        public string accessibleFieldsWithLowerCase = "AccessibleField";
+
+        //[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1304:NonPrivateReadonlyFieldsMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        public string FieldWithUpperCase;
+                   static void Main(string[] args)
         {
             Uri uri = new Uri("file:///C:/Program Files/ThisIsMyAddIn/ThisIsMyAddin.vsto|vstolocal");
             Console.WriteLine(uri.ToString());

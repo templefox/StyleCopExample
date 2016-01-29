93651a56b7ee428f2e4cfdf81ec6efc4a1a47fea
templefox <unboundfox@163.com>
2016-01-29 22:47:52 +0800

    first

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("file:///C:/Program Files/ThisIsMyAddIn/ThisIsMyAddin.vsto|vstolocal");
+            Console.WriteLine(uri.ToString());
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        public event Action<int> eventWithoutUpperCase;
+
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1303:ConstFieldNamesMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        public const string constWithLowerCase = "CONST";
+
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1307:AccessibleFieldsMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        public string accessibleFieldsWithLowerCase = "AccessibleField";
+        
+        //[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1304:NonPrivateReadonlyFieldsMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        public string FieldWithUpperCase;
+        void hi()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
